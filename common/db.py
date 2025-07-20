128e930c31c65abe9b5aa7b39c7535267d20b83e
sgajbi <sandeepgajbi@yahoo.com>
2025-07-21 00:12:54 +0800

    refactor: Consolidate SQLAlchemy Base to a single source

--- common/db.py
+++ common/db.py
@@ -1,14 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 from common.config import POSTGRES_URL
+from common.database_models import Base  # Import Base from database_models
 
 # Database setup
 SQLALCHEMY_DATABASE_URL = POSTGRES_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Define Base here to be imported by all SQLAlchemy models
-Base = declarative_base()
 
 def get_db_session():
     """
