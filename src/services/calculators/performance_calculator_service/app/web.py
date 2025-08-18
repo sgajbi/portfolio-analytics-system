ecb9eca23e0c800115db601bd346876cf16018ce
sgajbi <sandeepgajbi@yahoo.com>
2025-08-18 20:38:23 +0800

    refactor(perf): Remove unused performance-calculator-service

--- src/services/calculators/performance_calculator_service/app/web.py
+++
@@ -1,9 +0,0 @@
-# src/services/calculators/performance_calculator_service/app/web.py
-from fastapi import FastAPI
-from portfolio_common.health import create_health_router
-
-app = FastAPI(title="Performance Calculator - Health")
-
-# This service will depend on the database.
-health_router = create_health_router('db')
-app.include_router(health_router)
